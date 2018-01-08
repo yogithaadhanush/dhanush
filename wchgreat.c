cb3eebaefb210735820b999e9dafa9bce3559611
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-01-08 16:50:10 +0530

    Create wchgreat.c

---
+++ wchgreat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=1,b=2,c=3;
+if(a>b&&a>c)
+{
+	printf("1");
+}
+else
+{
+	if(b>a&&b>c)
+	{
+		printf("2");
+	}
+	else
+	{
+		printf("3");
+	}
+}
+return 0;
+}
