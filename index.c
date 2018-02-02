e2c7655bf96a93ee315f98ab60ea7e203c470444
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-02-02 16:06:34 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,a[i];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d%d\n",a[i],i);
+	}
+	return 0;
+}
