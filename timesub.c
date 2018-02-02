ad48ae3432b687d7cd6f08f6ed50e386e323a81a
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-02-02 15:51:30 +0530

    Update timesub.c

--- timesub.c
+++ timesub.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a,b,c,d,e,f;
-scanf("%d %d",&a,&b);
-scanf("%d %d",&c,&d);
+scanf("%d:%d",&a,&b);
+scanf("%d:%d",&c,&d);
 e=a-c;
 f=b-d;
-printf("the time is %d %d",e,f);
+printf("the time is %d:%d",e,f);
 return 0;
 }
