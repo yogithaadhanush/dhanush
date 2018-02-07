614576207c0190261d2113dc78da37dd8922a7d0
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-02-07 16:18:09 +0530

    Create numlines.c

---
+++ numlines.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    char str[100];
+    scanf("%[^\n]",str);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]=='.')
+            count++;    
+    }
+    printf("%d\n",count+1);
+    return 0;
+}
