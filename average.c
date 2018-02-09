ac43f3611bff463f86b9b84f7348abcca5d16d3e
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-02-09 16:13:24 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int n,i,sum=0,a[50],avg;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<=n;i++)
+	{
+	sum=sum+a[i];
+	}
+	avg=sum/n;
+	printf("%d",avg);
+		return 0;
+}
