a8069ad2d6785235649fb699fc1de6a77e460bf3
yogithaadhanush <31506077+yogithaadhanush@users.noreply.github.com>
2018-03-19 16:56:49 +0530

    Create 88lcm.c

---
+++ 88lcm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2,i,g,l;
+    scanf("%d %d",&n1,&n2);
+    for(i=1;i<=n1&&i<=n2;i++)
+    {
+           if(n1%i==0&&n2%i==0)
+            g=i;
+    }
+    l=(n1*n2)/g;
+    printf("%d",l);
+return 0;
+}
